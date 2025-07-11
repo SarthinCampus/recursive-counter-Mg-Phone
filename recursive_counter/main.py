862f52940ac6adcfe95b61cef0ea2ed02c083459
Mg Phone <khantalfred832@gmail.com>
2025-07-11 18:16:40 +0700

    Update main.py

--- recursive_counter/main.py
+++ recursive_counter/main.py
@@ -5,13 +5,10 @@ def countdown(n):
     countdown(n - 1)
 
 def run_countdown_program():
-    while True:
-        num_str = input("Enter a non-negative number to start countdown: ")
-        if num_str.isdigit():
-            countdown(int(num_str))
-            break
-        else:
-            print("Error")
+    num_str = input("Enter a non-negative number to start countdown: ")
+    if num_str.isdigit():
+        countdown(int(num_str))
+    else:
+        print("Error")
 
 run_countdown_program()
-
